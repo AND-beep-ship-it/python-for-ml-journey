6633309e30ef558fecacc4bff120f0686a917b9a
AND-beep-ship-it <anandronaldo1806@gmail.com>
2026-02-02 23:36:04 +0530

    Add files via upload

---
+++ Hands-on Practice.py
@@ -0,0 +1,8 @@
+character_name = ("Supriya Chaubey")
+character_age = "25"
+print("I Love You" + " " + character_name + " " + "Jiski umar" + " " + character_age + " saal hai" + " kya aap mujhse shaadi krengi??")
+P = 1000
+R = 5
+T = 2
+SI = (P * R * T) / 100
+print(SI)
---
+++ app.py
@@ -0,0 +1,16 @@
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+character_name = "George"
+character_age = "70"
+print("There once was a man named" + " " + character_name + ",")
+print("he was" + " " + character_age + " years old")
+print("He really liked the name george")
+print("but didn't liked being" + " " + character_age )
+phrase = "Giraffe Academy"
+print(phrase + " " + "is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(len(phrase))
