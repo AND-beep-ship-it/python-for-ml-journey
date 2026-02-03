3d46d2367c45a3336370402c35c1deac1f582e38
AND-beep-ship-it <anandronaldo1806@gmail.com>
2026-02-03 12:36:09 +0530

    app.py

--- app.py
+++ app.py
@@ -14,3 +14,36 @@ print(phrase.lower())
 print(phrase.upper())
 print(phrase.isupper())
 print(len(phrase))
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name +  "!" + " You are " + age + " years old")
+
+#calculator
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+Sum = num1 + num2
+print(Sum)
+
+#MadLibs Game
+color = input("Enter your color: ")
+plural_noun = input("Enter your plural noun: ")
+celebrity = input("Enter your celebrity: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+
+#Tuples
+coordinates = (4, 5)
+print(coordinates[0])
+
+#Functions
+def say_hi():
+    print("Hello " + name + "!")
+
+say_hi()
+
+#Return
+def cube(num):
+    return num*num*num
+
+print(cube(4))
